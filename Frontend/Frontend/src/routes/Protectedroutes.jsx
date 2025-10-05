cb4920f9f7f139ef3d3e6c469bc05fbad9c67e80
CYB101NAME <CYB101EMAIL>
2025-10-05 13:02:08 -0500

    Backup before cleaning folder structure

--- Frontend/Frontend/src/routes/Protectedroutes.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Navigate, Outlet , useLocation} from "react-router-dom";
-const ProtectedRoute = ({ requireRole }) => {
-  return <Outlet />;
-}
-export default ProtectedRoute;
